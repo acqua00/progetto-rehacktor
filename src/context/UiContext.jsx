133cbdd8c6f02692f877cc11a48992b5c890771c
Francesco Acquafredda <francesco.acquafreda@gmail.com>
2025-11-18 17:50:35 +0100

    add correct UiContext file

---
+++ src/context/UiContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+const UiContext = createContext();
+
+export function UIProvider({ children }) {
+  const [resetGenres, setResetGenres] = useState(false);
+
+  const triggerGenreReset = () => {
+    setResetGenres(true);
+    setTimeout(() => setResetGenres(false), 50);
+  };
+
+  return (
+    <UiContext.Provider value={{ resetGenres, triggerGenreReset }}>
+      {children}
+    </UiContext.Provider>
+  );
+}
+
+export default UiContext;
